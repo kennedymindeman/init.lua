2de37032278871919b2a0ef11cd0594781033caa
Kennedy Mindeman <kmindeman@corp.doosan.com>
2022-11-16 10:40:03 -0600

    I'm not really sure

--- packer.lua
+++ packer.lua
@@ -1,13 +1,26 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- have to source this first
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use {
-        'neoclide/coc.nvim', branch = 'release'
-    }
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'sainnhe/gruvbox-material'
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
+  use 'wbthomason/packer.nvim'
+  use {
+    'neoclide/coc.nvim', branch = 'release'
+  }
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'sainnhe/gruvbox-material'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+     requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
