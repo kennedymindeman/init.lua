324f3a8fdabe89eb5e807a5a33bbb99bf715c1ff
Kennedy Mindeman <kmindeman@corp.doosan.com>
2022-11-16 11:31:16 -0600

    Change file heirchy

--- init.lua
+++ init.lua
@@ -1,9 +1,4 @@
-require 'packer'
+require '_packer'
 require 'settings'
-require 'treesitter'
-require 'telescope'
---local builtin = require('telescope.builtin')
---vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
---vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
---vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
---vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+require '_treesitter'
+require '_telescope'
